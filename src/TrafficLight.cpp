ccdcd63f1a7eb3577174ffe62fb5d0d89268349d
lulezo <moiz.hassan.mh@gmail.com>
2023-08-22 02:33:28 -0400

    Update TrafficLight.cpp

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -13,8 +13,8 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
-    std::unique_lock<std::mutex> rlock(_mutex);
-    _cond.wait(rlock, [this](){ return !_queue.empty(); });
+    std::unique_lock<std::mutex> lock(_mutex);
+    _cond.wait(lock, [this](){ return !_queue.empty(); });
     T msg = std::move(_queue.back());
     _queue.pop_back();
     return msg;
@@ -26,7 +26,7 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
-    std::lock_guard<std::mutex> slock(_mutex);
+    std::lock_guard<std::mutex> lock(_mutex);
     _queue.push_back(std::move(msg));
     _cond.notify_one();
 }
@@ -70,30 +70,26 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
     
-    // initalize variables
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-    double cycleDuration = 4000 + std::rand() % 3000; // duration of a single cycle in ms (between 4 and 6 seconds)
+    // Initialize variables
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(40, 60); // Random duration between 4 and 6 seconds
+    double cycleDuration = dis(gen);
     std::chrono::time_point<std::chrono::system_clock> lastUpdate;
 
-    // init stop watch
+    // Init stop watch
     lastUpdate = std::chrono::system_clock::now();
-    while (true)
+    while(true)
     {
-        // sleep at every iteration to reduce CPU usage
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        
-        // compute time difference to stop watch
         long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
         if (timeSinceLastUpdate >= cycleDuration) 
-        {
             // Toggle between red and green
-            _currentPhase = (_currentPhase == TrafficLightPhase::red ) ? TrafficLightPhase::green : TrafficLightPhase::red;
-            
-            // Send update message to the queue
-            _msg.send(std::move(_currentPhase));
+        {
+            _currentPhase = (_currentPhase == TrafficLightPhase::red) ? TrafficLightPhase::green : TrafficLightPhase::red;
         }
-        
-        // reset stop watch for next cycle
-        lastUpdate = std::chrono::system_clock::now();
+        _msg.send(std::move(_currentPhase));
+        cycleDuration = dis(gen); // Generate a new cycle duration
+        lastUpdate = std::chrono::system_clock::now(); // Reset lastUpdate after phase change
     }
 }
